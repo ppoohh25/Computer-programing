c34b06c1bda3ae769e1a24225b65d905d2d571e9
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2023-01-25 07:05:04 +0700

    Update 5.c

--- 5.c
+++ 5.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int live = 20599;
+    /*int live = 20599;
     //September 10, 1964
     int a = 20;
     
@@ -23,8 +23,17 @@ int main()
     
     live = live + a;
     
+    printf("Jack Ma has been living in this world for %d years, %d month, and %d days",year,month,live);*/
+    
+    int live = 20599;
+    int year = live/365;
+    live = live%365;
+    
+    int month = live/30;
+    live = live%30;
+    
     printf("Jack Ma has been living in this world for %d years, %d month, and %d days",year,month,live);
     
     
     return 0;
-}
+
