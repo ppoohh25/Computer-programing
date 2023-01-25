766ce360a3b0d3bb9bbf587f2b0ded88512cac1c
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2023-01-25 07:08:52 +0700

    Update 4.c

--- Week 2/4.c
+++ Week 2/4.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+#define PI 3.14
+int main()
+{
+    float d = 12742.00;
+    float r = d/2;
+    float cc = 2*PI*r;
+    float ca = PI*r*r;
+    float cv = 4/3*PI*r*r*r;
+    float cs = 4*PI*r*r;
+    printf("The world has %.2f km of diameter, %.2f km of circumference, %.2f km^2 of area, %.2f km^3 of volume, and %.2f km^2 of surface area.",d,cc,ca,cv,cs);
+    return 0;
+}
