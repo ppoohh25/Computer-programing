636028222475d7d027dbcede3c9d548f40c9df4c
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2023-02-07 13:31:22 +0700

    Create 2.c

---
+++ array and string/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j;
+    printf("Please enter number 1: ");
+    scanf("%d",&a);
+    printf("Please enter number 2: ");
+    scanf("%d",&b);
+    printf("Please enter number 3: ");
+    scanf("%d",&c);
+    printf("Please enter number 4: ");
+    scanf("%d",&d);
+    printf("Please enter number 5: ");
+    scanf("%d",&e);
+    printf("Please enter number 6: ");
+    scanf("%d",&f);
+    printf("Please enter number 7: ");
+    scanf("%d",&g);
+    printf("Please enter number 8: ");
+    scanf("%d",&h);
+    printf("Please enter number 9: ");
+    scanf("%d",&i);
+    printf("Please enter number 10: ");
+    scanf("%d",&j);
+    printf("The number are %d, %d, %d, %d, %d, %d, %d, %d, %d, %d",a,b,c,d,e,f,g,h,i,j);
+    return 0;
+}
