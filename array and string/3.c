456ec9ef7857d2af09e4248ab8463d154604ec45
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2023-02-07 14:03:49 +0700

    Update 3.c

--- array and string/3.c
+++ array and string/3.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
 int main()
 {
+    char line[100];
     int num[10];
     for(int i =0;i<10;i++){
         printf("Please enter number %d: ",i+1);
-        scanf("%d",&num[i]);
+        fgets(line,sizeof(line),stdin);
+        sscanf(line,"%d",&num[i]);
     }
     printf("The number are %d, %d, %d, %d, %d, %d, %d, %d, %d, %d",num[0],num[1],num[2],num[3],num[4],num[5],num[6],num[7],num[8],num[9]);
     return 0;
