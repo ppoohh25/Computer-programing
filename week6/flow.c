1f646e2ff752e6cd400c31a679ace6245d26031d
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2023-02-21 12:54:25 +0700

    Create flow.c

---
+++ week6/flow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  int x = 7;
+  int y = 9;
+
+  if(x==y){
+    printf("x = %d y = %d",x+1,y);
+  }
+  else{
+    printf("x = %d y = %d",y*7,y+7);
+  }
+  return 0;
+}
